022f6dbc7126dee09ee0c051f9538de695d93cc8
AntiANT8406 <AntiANT8406@yandex.ru>
2024-09-17 20:48:35 +0300

    fix(arrowbutton.tsx): исправление из репы наставника

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,13 +5,18 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type ArrowButtonProps = {
+	onClick: OnClick;
+	isOpen: boolean;
+};
+
+export const ArrowButton = ({ onClick }: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
+			onClick={onClick}
 			className={styles.container}>
 			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
 		</div>
