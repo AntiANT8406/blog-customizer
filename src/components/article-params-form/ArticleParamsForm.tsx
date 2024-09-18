5efa5c0e1808eabf5499c28be398feafa02d10ca
AntiANT8406 <AntiANT8406@yandex.ru>
2024-09-18 20:13:03 +0300

    feat(ArticleParamsForm.tsx, useModalClosure): :sparkles: Добавлен хук для закрытия по щелчку вне формы и Esc

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,15 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useModalClosure } from 'src/hooks/useModalClosure';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const closureRef = useRef(null);
+	useModalClosure({ ref: closureRef, isOpen, close: () => setIsOpen(false) });
 	return (
 		<>
 			<ArrowButton
@@ -16,7 +19,8 @@ export const ArticleParamsForm = () => {
 				}}
 			/>
 			<aside
-				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}
+				ref={closureRef}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
